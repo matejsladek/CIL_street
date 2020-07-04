b62fa87a35655027d3de7e4af24c838090e04920
Marco Bagatella <mbagatella@lo-login-02.leonhard.ethz.ch>
2020-07-04 15:58:05 +0200

    add stop on metric

--- train.py
+++ train.py
@@ -121,17 +121,15 @@ def create_and_train_model(train_dataset, val_dataset_original, val_dataset_nump
     class CustomCallback(tf.keras.callbacks.Callback):
         def __init__(self):
             super(CustomCallback, self).__init__()
-            self.lowest_loss = 100
-            self.metric_index = 0
+            self.lowest_loss = 0
+            self.metric_index = 2
             if config['predict_contour'] or config['predict_distance']:
-                self.metric_index = 1
+                self.metric_index = -3
 
         def on_epoch_end(self, epoch, logs=None):
             loss = model.evaluate(x=val_dataset_numpy_x, y=val_dataset_numpy_y, verbose=0)
             print('\nValidation metrics: ' + str(loss))
-            loss2 = kaggle_metric(model.predict(val_dataset_numpy_x)[0], val_dataset_numpy_y[0])
-            print('Loss2: ' + str(loss2))
-            if loss[self.metric_index] < self.lowest_loss:
+            if loss[self.metric_index] > self.lowest_loss:
                 self.lowest_loss = loss[self.metric_index]
                 print('New lowest loss. Saving weights.')
                 model.save_weights(config['log_folder'] + '/best_model.h5')
@@ -140,9 +138,6 @@ def create_and_train_model(train_dataset, val_dataset_original, val_dataset_nump
         tf.keras.callbacks.TensorBoard(config['log_folder'] + '/log'),
         CustomCallback(),
     ]
-
-    loss2 = kaggle_metric(model.predict(val_dataset_numpy_x)[0], val_dataset_numpy_y[0])
-    print('Loss2: ' + str(loss2))
 
     model_history = model.fit(train_dataset, epochs=config['epochs'],
                               steps_per_epoch=steps_per_epoch,
