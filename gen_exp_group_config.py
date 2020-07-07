f293879c5dd04d4e2e536561e4f905493dfadd1e
hanyao8 <choong.hanyao@gmail.com>
2020-07-07 15:02:47 +0200

    add table2 diffs and organize gen_confs script

--- gen_exp_group_config.py
+++ gen_exp_group_config.py
@@ -2,17 +2,18 @@ import json
 import os
 import argparse
 
+project_dir = ""
+internal_baseline_path = os.path.join(*[project_dir,"config_archive","internal_baseline.json"])
+internal_baseline = json.loads(open(internal_baseline_path,'r').read())
 
-#official
-def gen_table1(base):
-    exp_group_name = 'exp_group_table1'
-    exp_group_diff = {'name':os.path.join(exp_group_name,'exp')}
-    exp_diff_list = [
-            {'dataset':'original_all','preprocess':False},
-            {'dataset':'original_all','preprocess':True},
-            {'dataset':'maps1800_all','preprocess':False},
-            {'dataset':'maps1800_all','preprocess':True}
-            ]
+good_model_path = os.path.join(*[project_dir,"config_archive","good_model_0707.json"])
+good_model = json.loads(open(good_model_path,'r').read())
+
+best_model_path = os.path.join(*[project_dir,"config_archive","best_model_0707.json"])
+best_model = json.loads(open(best_model_path,'r').read())
+
+
+def diff_to_conf(base,exp_diff_list,exp_group_diff):
     config_list = []
     for exp_diff in exp_diff_list:
         config_tmp = base.copy()
@@ -23,6 +24,43 @@ def gen_table1(base):
         config_list.append(config_tmp)
     return(config_list)
 
+
+#official
+def gen_table1_configs(base):
+    exp_group_name = 'exp_group_table1'
+    exp_group_diff = {'name':os.path.join(exp_group_name,'exp')}
+    exp_diff_list = [
+            {'dataset':'original_all',
+                'v_flip':0,'h_flip':0,'rot':0,'contrast':0,'brightness':0},
+            {'dataset':'maps1800_all',
+                'v_flip':0,'h_flip':0,'rot':0,'contrast':0,'brightness':0},
+            {'dataset':'original_all'},
+            {'dataset':'maps1800_all'}
+            ]
+#    exp_diff_list = [
+#            {'dataset':'original_all','preprocess':False},
+#            {'dataset':'original_all','preprocess':True},
+#            {'dataset':'maps1800_all','preprocess':False},
+#            {'dataset':'maps1800_all','preprocess':True}
+#            ]
+    config_list = diff_to_conf(base,exp_diff_list,exp_group_diff)
+    return config_list
+
+
+def gen_table2_configs(base):
+    exp_group_name = 'exp_group_table2'
+    exp_group_diff = {'name':os.path.join(exp_group_name,'exp')}
+    exp_diff_list = [
+            {'backbone':'resnet101','se':False},
+            {'pretrained':False},
+            {'predict_distance':False,'predict_contour':False},
+            {'n_ensemble':0},
+            {'postprocess':'none'},
+            ]
+    config_list = diff_to_conf(base,exp_diff_list,exp_group_diff)
+    return config_list
+
+
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-g','--exp_group',default=None)
@@ -30,24 +68,18 @@ if __name__=="__main__":
     argsdict = vars(args)
     print(argsdict)
 
-    project_dir = ""
-    internal_baseline_path = os.path.join(*[project_dir,"config_archive","internal_baseline.json"])
-    internal_baseline = json.loads(open(internal_baseline_path,'r').read())
-    best_model_path = os.path.join(*[project_dir,"config_archive","best_model_0707.json"])
-    best_model = json.loads(open(best_model_path,'r').read())
-#    intermediate_baseline_path = os.path.join(*[project_dir,"config_archive","intermediate_baseline.json"])
-#    intermediate_baseline = json.loads(open(int_base_path,'r').read())
-#    intermediate_baseline_path = os.path.join(*[project_dir,"config_archive","intermediate_baseline.json"])
-#    intermediate_baseline = json.loads(open(int_base_path,'r').read())
-
-
     if argsdict['exp_group'] == 'table1':
-        config_list = gen_table1(best_model)
+        config_list = gen_table1_configs(good_model)
+    elif argsdict['exp_group'] == 'table2':
+        config_list = gen_table2_configs(best_model)
+
 
     exp_group_config_path = os.path.join(project_dir,'config_'+argsdict['exp_group'])
     if os.path.exists(exp_group_config_path):
         shutil.rmtree(exp_group_config_path)
     os.makedirs(exp_group_config_path)
+
+    
     for i in range(len(config_list)):
         out_file_path = os.path.join(exp_group_config_path,"exp"+str(i)+".json")
         out_file = open( out_file_path, "w") 
